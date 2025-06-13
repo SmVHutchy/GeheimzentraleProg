adb519bd5d6ca32ab3166c7c5c69b50dd27d7f63
Liam Moosburger <135737415+SmVHutchy@users.noreply.github.com>
2025-06-13 10:52:47 +0200

    Update Kotlin to 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.compose") version "1.5.11" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
 }
@@ -13,6 +13,6 @@ buildscript {
     }
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
